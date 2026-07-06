2675d70f9ffd6c828493016c6f0cb0f3297b277b
tuongviltv-oss <tuongviltv@gmail.com>
2026-07-06 10:14:20 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/gocvuonggockhongvuong/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
